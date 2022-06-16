7491f8dbc8127fd79ef7eb43e4a75924b71cc7d2
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2022-06-16 16:23:49 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC app from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
+            ViewData["Student"] = "Bryan Dietrich Bernhardt";
 
             return View();
         }
