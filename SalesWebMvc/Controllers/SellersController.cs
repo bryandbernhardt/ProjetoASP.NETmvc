96bc3085c8df0e4c8fea90614d8135b8f609d79c
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2022-07-30 22:41:56 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -82,8 +82,15 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Don't delete seller because he/she has sales" });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
